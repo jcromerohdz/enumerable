e12c9429c1a39e48515cb52e468cd21c1341d14c
J. Christian Romero <jcromerohdz@gmail.com>
2019-10-16 09:40:29 -0700

    Fixing rubocop issues

--- test.rb
+++ test.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 require './enumerable'
+# rubocop:disable  Metrics/AmbiguousBlockAssociation
 
-puts %w[h i hi hello].each do |element| puts element end
+puts %w[h i hi hello].each { |element| puts element }
 puts %w[h i hi hello].my_each { |element| puts element }
 
 puts %w[h i hi hello].each_with_index { |element, index| puts element + index.to_s }
