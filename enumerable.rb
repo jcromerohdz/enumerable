715d000a2b8e85fbb7a34548c18861b2580f77af
J. Christian Romero <jcromerohdz@gmail.com>
2019-10-16 08:33:28 -0700

    Fixing enumerable method for rubocop

--- enumerable.rb
+++ enumerable.rb
@@ -1,64 +1,128 @@
 # frozen_string_literal: true
 
-module Enumerable
+module Enumerable # rubocop:disable Metrics/ModuleLength
   def my_each
-    index = 0
-    while index < self.count
-      yield(self[index])
-      index += 1
+    return to_enum unless block_given?
+
+    i = 0
+    while i < size
+      yield(self[i])
+      i += 1
     end
+    self
   end
 
   def my_each_with_index
-    index = 0
-    while index < self.count
-      yield(self[index], index)
-      index += 1
+    return to_enum unless block_given?
+
+    i = 0
+    while i < size
+      yield(self[i], i)
+      i += 1
     end
   end
 
   def my_select
-    my_list = []
-    self.my_each { |element| my_list << element if yield(element) }
-    my_list
+    return to_enum unless block_given?
+
+    item_select = []
+    my_each { |i| item_select << i if yield(i) }
+    item_select
   end
 
-  def my_all?
-    result = true
-    self.my_each { |element| result = false unless yield(element) }
-    result
-  end
-
-  def my_any?
-    result = false
-    self.my_each { |element| result = true if yield(element) }
-    result
-  end
-
-  def my_none?
-    result = true
-    self.my_each { |element| result = false if yield(element) }
-    result
-  end
-
-  def my_count
-    total = 0
-    self.my_each { total += 1 }
-    total
-  end
-
-  def my_map(my_proc = nil)
-    result = []
-    unless my_proc == nil
-      self.my_each { |element| result << (my_proc.call element) }
+  # rubocop:disable  Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+  def my_all?(param = nil)
+    if block_given?
+      my_each { |i| return false unless yield(i) }
+    elsif param.class == Class
+      my_each { |i| return false unless i.class == param }
+    elsif param.class == Regexp
+      my_each { |i| return false unless i =~ param }
+    elsif param.nil?
+      my_each { |i| return false unless i }
     else
-      self.my_each { |element| result << yield(element) }
+      my_each { |i| return false unless i == param }
     end
-    result
+    true
   end
 
-  def my_inject(start_value)
-    self.my_each { |element| start_value = yield(start_value, element) }
-    start_value
+  def my_any?(param = nil)
+    if block_given?
+      my_each { |i| return true if yield(i) }
+    elsif param.class == Class
+      my_each { |i| return true if i.class == param }
+    elsif param.class == Regexp
+      my_each { |i| return true if i =~ param }
+    elsif param.nil?
+      my_each { |i| return true if i }
+    else
+      my_each { |i| return true if i == param }
+    end
+    false
+  end
+
+  def my_none?(param = nil)
+    if block_given?
+      my_each { |i| return false if yield(i) }
+    elsif param.class == Class
+      my_each { |i| return false if i.class == param }
+    elsif param.class == Regexp
+      my_each { |i| return false if i =~ param }
+    elsif param.nil?
+      my_each { |i| return false if i }
+    else
+      my_each { |i| return false if i == param }
+    end
+    true
+  end
+
+  def my_count(items = nil)
+    count = 0
+    if block_given?
+      my_each { |i| count += 1 if yield(i) == true }
+    elsif items.nil?
+      my_each { count += 1 }
+    else
+      my_each { |i| count += 1 if i == items }
+    end
+    count
+  end
+
+  def my_map(arg = nil)
+    return to_enum unless block_given?
+
+    arr = []
+    my_each do |i|
+      arr << if !arg.nil?
+        arg.call(i)
+      else
+        yield(i)
+      end
+    end
+    arr
+  end
+
+  def my_inject(*args)
+    arr = to_a.dup
+    if args[0].nil?
+      operand = arr.shift
+    elsif args[1].nil? && !block_given?
+      symbol = args[0]
+      operand = arr.shift
+    elsif args[1].nil? && block_given?
+      operand = args[0]
+    else
+      operand = args[0]
+      symbol = args[1]
+    end
+
+    arr[0..-1].my_each do |i|
+      operand = if symbol
+                  operand.send(symbol, i)
+                else
+                  yield(operand, i)
+                end
+    end
+    operand
   end
 end
